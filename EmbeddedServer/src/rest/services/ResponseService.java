26309e19d537cc112ca2c6acc48513c40851219d
Hassan-Qureshi <alih.qureshi@hotmail.com>
2018-09-12 01:58:18 +0500

    Updates

--- EmbeddedServer/src/rest/services/ResponseService.java
+++ EmbeddedServer/src/rest/services/ResponseService.java
@@ -2,15 +2,12 @@
  * 
  */
 package rest.services;
-import javax.sound.midi.Track;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import DBController.DBOper;
 import pojo.Responses;
 /**
  * @author AliHassan
@@ -20,20 +17,7 @@ import pojo.Responses;
 
 @Path("/Response")
 public class ResponseService {
-	
-	
-	
-	@POST
-	@Path("/post")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createTrackInJSON(Track track) {
-
-		String result = "Track saved : " + track;
-		return Response.status(201).entity(result).build();
 		
-	}
-	
-	
 	@POST
 	@Path("/saveResponse")
     @Produces({MediaType.APPLICATION_JSON})
